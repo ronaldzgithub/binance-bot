ed099bec7acd83ef329172f8eeb490f4f92314c7
kgeronim <kevin.geronimo@outlook.com>
2020-02-16 15:13:27 +0000

    added environment variables for secrets

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,70 @@
 use futures::{join, stream::StreamExt, try_join};
-use log::{info, warn, Level};
+use log::{info, warn, error, Level};
 use rust_decimal::{prelude::*, Decimal};
-//use clap::{Arg, App};
-use tokio_binance::*;
+use clap::{Arg, App};
 use serde_json::Value;
+use tokio_binance::*;
 
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // let matches = App::new("binance-bot")
-    // .version("0.1.0")
-    // .author("kgeronim <kevin.geronimo@outlook.com>")
-    // .about("Binance MACD-RSI Mean Reversion Trading Bot")
-    // .arg(Arg::with_name("interval")
-    //     .short("i")
-    //     .long("granularity")
-    //     .takes_value(true)
-    //     .possible_values(&["1m", "3m", "5m", "15m", "30m", "1h", "2h", "4h", "6h", "8h", "12h", "1d", "3d", "1w", "1M"]))
-    // .arg(Arg::with_name("SYMBOL")
-    //     .help("Trading pair that will be used for trading")
-    //     .required(true)
-    //     .index(1))
-    // .arg(Arg::with_name("RSI-BUY")
-    //     .help("Buy at or above the provided rsi")
-    //     .required(true)
-    //     .index(3)
-    //     .validator(binance_bot::validator))
-    // .arg(Arg::with_name("RSI-SELL")
-    //     .help("Sell at or below the provided rsi")
-    //     .required(true)
-    //     .index(4)
-    //     .validator(binance_bot::validator))
-    // .arg(Arg::with_name("debug")
-    //     .short("d")
-    //     .help("Print debug information verbosely"))
-    // .get_matches();
+    let matches = App::new("binance-bot")
+        .version("0.1.0")
+        .author("kgeronim <kevin.geronimo@outlook.com>")
+        .about("Binance MACD-RSI Mean Reversion Trading Bot")
+        .arg(Arg::with_name("interval")
+            .short("i")
+            .long("interval")
+            .takes_value(true)
+            .possible_values(&["1m", "3m", "5m", "15m", "30m", "1h", "2h", "4h", "6h", "8h", "12h", "1d", "3d", "1w", "1M"]))
+        .arg(Arg::with_name("SYMBOL")
+            .help("Trading pair that will be used for trading")
+            .required(true)
+            .index(1))
+        .arg(Arg::with_name("RSI-BUY")
+            .help("Buy at or above the provided rsi")
+            .required(true)
+            .index(2)
+            .validator(binance_bot::validator))
+        .arg(Arg::with_name("RSI-SELL")
+            .help("Sell at or below the provided rsi")
+            .required(true)
+            .index(3)
+            .validator(binance_bot::validator))
+        .arg(Arg::with_name("debug")
+            .short("d")
+            .help("Print debug information verbosely"))
+        .get_matches();
 
-    simple_logger::init_with_level(Level::Info).unwrap();
-    let _api_key = "D3XovajhBd4mS0YIEwlPNpkituPvL2LCC1JogGLAd4djXZasUnbRthwUEwimtrOF";
-    let _secret_key = "B2w5fAlmRBus9sxBYq5VyMLe69DiamCaX1WrSjXakhPEODtD2gEp3R1IaaKIHyNI";
+    if matches.is_present("debug") {
+        simple_logger::init_with_level(Level::Debug).unwrap();
+    } else {
+        simple_logger::init_with_level(Level::Info).unwrap();
+    }
 
-    let symbol = "BNBUSDT";
-    let rsi_buy: Decimal = 30.into();
-    let rsi_sell: Decimal = 70.into();
+    let symbol = matches.value_of("SYMBOL").unwrap();
+    let rsi_buy = matches.value_of("RSI-BUY").map(|x| x.parse::<Decimal>().unwrap()).unwrap();
+    let rsi_sell = matches.value_of("RSI-SELL").map(|x| x.parse::<Decimal>().unwrap()).unwrap();
+    let interval = matches.value_of("interval").map_or(Interval::OneHour, |x| {
+        serde_json::from_value(Value::String(x.into())).unwrap()
+    });
 
-    let client = AccountClient::connect(_api_key, _secret_key, BINANCE_US_URL)?;
+    if rsi_buy >= rsi_sell {
+        error!("Buy low sell high not the other way around...");
+        panic!()
+    }
+
+    let api_key = if let Ok(val) = std::env::var("API_KEY") { val } else {
+        error!("API_KEY environment variable required");
+        panic!() 
+    };
+
+    let secret_key = if let Ok(val) = std::env::var("SECRET_KEY") { val } else {
+        error!("SECRET_KEY environment variable required");
+        panic!() 
+    };
+
+    let client = AccountClient::connect(&api_key, secret_key, BINANCE_US_URL)?;
 
     let info = client
         .to_general_client()
@@ -52,11 +72,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .json::<Value>()
         .await?;
 
-    let (lot_size, min_notional, base_asset, quote_asset) = binance_bot::process_info("BNBUSDT", &info);
+    if binance_bot::symbol_exist(symbol, &info) {
+        if !symbol.contains("USD") {
+            error!("Quote Asset must be USD or a stable coin");
+            panic!()
+        }
+    } else {
+        error!("Invalid Symbol. Base and quote assets are concatinated. e.g. BNBUSDT");
+        panic!()
+    }
+
+    let (lot_size, min_notional, base_asset, quote_asset) = binance_bot::process_info(symbol, &info);
     info!("lot_size: {}, min_notional: {}, base_asset: {}, quote_asset: {}", lot_size, min_notional, base_asset, quote_asset);
 
-    let mut rsi = binance_bot::rsi(symbol, Interval::OneHour, _api_key).await?;
-    let mut macd = binance_bot::macd(symbol, Interval::OneHour, _api_key).await?;
+    let mut rsi = binance_bot::rsi(symbol, interval, &api_key).await?;
+    let mut macd = binance_bot::macd(symbol, interval, &api_key).await?;
 
     let mut buy_state = binance_bot::OrderState {
         prev_macd: None,
